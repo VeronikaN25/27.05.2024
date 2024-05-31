349d9a027d2ac31687fe757b00a7ba805906756e
Veeя <novichikhina.05@mail.ru>
2024-05-31 10:41:08 +0500

    Add project files.

---
+++ 27.05.2024/Form2.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace _27._05._2024
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_word = new System.Windows.Forms.Label();
+            this.textBox_guess = new System.Windows.Forms.TextBox();
+            this.button_next = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.label_result = new System.Windows.Forms.Label();
+            this.label_score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_word
+            // 
+            this.label_word.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label_word.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_word.Location = new System.Drawing.Point(48, 42);
+            this.label_word.Name = "label_word";
+            this.label_word.Size = new System.Drawing.Size(329, 65);
+            this.label_word.TabIndex = 0;
+            this.label_word.Text = "Word";
+            this.label_word.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_guess
+            // 
+            this.textBox_guess.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_guess.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_guess.Location = new System.Drawing.Point(48, 128);
+            this.textBox_guess.Name = "textBox_guess";
+            this.textBox_guess.Size = new System.Drawing.Size(329, 45);
+            this.textBox_guess.TabIndex = 1;
+            this.textBox_guess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button_next
+            // 
+            this.button_next.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_next.Location = new System.Drawing.Point(48, 203);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(329, 60);
+            this.button_next.TabIndex = 2;
+            this.button_next.Text = "Next";
+            this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_start.Location = new System.Drawing.Point(48, 286);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(329, 60);
+            this.button_start.TabIndex = 3;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // label_result
+            // 
+            this.label_result.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_result.Location = new System.Drawing.Point(48, 365);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(329, 66);
+            this.label_result.TabIndex = 4;
+            this.label_result.Text = "Result";
+            this.label_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_score
+            // 
+            this.label_score.AutoSize = true;
+            this.label_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_score.Location = new System.Drawing.Point(186, 461);
+            this.label_score.Name = "label_score";
+            this.label_score.Size = new System.Drawing.Size(46, 32);
+            this.label_score.TabIndex = 5;
+            this.label_score.Text = "00";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 515);
+            this.Controls.Add(this.label_score);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.button_next);
+            this.Controls.Add(this.textBox_guess);
+            this.Controls.Add(this.label_word);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_word;
+        private System.Windows.Forms.TextBox textBox_guess;
+        private System.Windows.Forms.Button button_next;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.Label label_score;
+    }
+}
---
+++ 27.05.2024/Form2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _27._05._2024
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            LoadWordsFromFile("words.txt");
+        }
+        string[] words;
+        int index = 0, score = 0;
+        Random random = new Random();
+        private void LoadWordsFromFile(string fileName)
+        {
+            try
+            {
+               
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+                if (File.Exists(filePath))
+                {
+                    words = File.ReadAllLines(filePath);
+                }
+                else
+                {
+                    MessageBox.Show($"File not found: {filePath}");
+                   
+                    words = new string[] { "default", "words", "in", "case", "of", "error" };
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading words from file: {ex.Message}");
+                words = new string[] { "default", "words", "in", "case", "of", "error" };
+            }
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            index = 0;
+            score = 0;
+            label_result.Text = "Result";
+            label_result.BackColor = Color.Peru;
+            label_score.Text = "00";
+            displayWord();
+            button_next.Enabled = true;
+            button_start.Enabled = false;
+        }
+
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            checkWord();
+            if (index < words.Length - 1)
+            {
+                index++;
+                displayWord();
+            }
+        }
+        public void displayWord()
+        {
+            int pos1 = random.Next(words[index].Length);
+            int pos2 = random.Next(words[index].Length);
+            int pos3 = random.Next(words[index].Length);
+
+            string word = words[index];
+
+            word = word.Remove(pos1, 1).Insert(pos1, "_");
+            word = word.Remove(pos2, 1).Insert(pos2, "_");
+            word = word.Remove(pos3, 1).Insert(pos3, "_");
+
+            label_word.Text = word;
+
+        }
+        public void checkWord()
+        {
+            if (textBox_guess.Text.ToLower().Equals(words[index]))
+            {
+                label_result.Text = "Correct";
+                label_result.BackColor = Color.Green;
+                score++;
+            }
+            else
+            {
+                label_result.Text = "Wrong";
+                label_result.BackColor = Color.Red;
+            }
+            if (index == words.Length - 1)
+            {
+                button_start.Enabled = true;
+                button_next.Enabled = false;
+            }
+            textBox_guess.Text = "";
+            label_score.Text = score.ToString() + " / " + words.Length.ToString();
+        }
+
+
+    }
+}
+
